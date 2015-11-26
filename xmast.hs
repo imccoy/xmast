bb9ab58e0969d9d0e8a2e2a3ec7b145742d985c5
Iain McCoy <iain@mccoy.id.au>
2015-11-26 19:18:55 +1100

    initial

---
+++ xmast.hs
@@ -0,0 +1,17 @@
+module Xmast where
+
+import Data.List (intersperse)
+
+trunk = "||"
+
+branch 0 = "/\\"
+branch n = "\\" ++ replicate n '_' ++ trunk ++ replicate n '_' ++ "/"
+
+centerlines :: [String] -> [String]
+centerlines lines = map pad lines
+  where maxlength = foldl (flip $ max . length) 0 lines
+        padSize line = (maxlength - (length line)) `div` 2
+        pad line = replicate (padSize line) ' ' ++ line
+
+main = do
+  mapM putStrLn $ centerlines $ (intersperse trunk [branch w | w <- [0,2..20]]) ++ replicate 3 trunk
