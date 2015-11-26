40a361481c7a111fca19c187c4c1f1c96669f61c
Iain McCoy <iain@mccoy.id.au>
2015-11-27 01:20:21 +1100

    move padding calculation

--- app/Main.hs
+++ app/Main.hs
@@ -9,13 +9,14 @@ data Tree next = Branch Int next
                | Done
   deriving (Functor)
 
-data STree = Peak | LBranch Int | STrunk | RBranch Int
+data STree = Pad Int | Peak | LBranch Int | STrunk | RBranch Int
 
 
 sTree Peak = "/\\"
 sTree STrunk = "||"
 sTree (LBranch n) = "\\" ++ replicate n '_'
 sTree (RBranch n) = replicate n '_' ++ "/"
+sTree (Pad n) = replicate n ' '
 
 type TreeM = Free Tree
 
@@ -25,24 +26,27 @@ branch n = liftF (Branch n ())
 trunk :: TreeM () 
 trunk = liftF (Trunk ())
 
-runList :: TreeM a -> [[STree]]
-runList (Free (Branch 0 k)) = [Peak]:(runList k)
-runList (Free (Branch n k)) = [LBranch n, STrunk, RBranch n]:(runList k)
-runList (Free (Trunk k)) = [STrunk]:(runList k)
-runList (Free (Done)) = []
-runList (Pure _) = []
+maxTreeBranch :: TreeM a -> Int
+maxTreeBranch (Free (Branch n k)) = max n (maxTreeBranch k) -- pretty good chance this is a space leak
+maxTreeBranch (Free (Trunk k)) = maxTreeBranch k
+maxTreeBranch _ = 0
 
-centerlines :: [String] -> [String]
-centerlines lines = map pad lines
-  where maxlength = foldl (flip $ max . length) 0 lines
-        padSize line = (maxlength - (length line)) `div` 2
-        pad line = replicate (padSize line) ' ' ++ line
+runList :: TreeM a -> [[STree]]
+runList t = runList' t
+  where
+    trunkLhsCol = length (sTree $ LBranch $ maxTreeBranch t)
+    runList' (Free (Branch 0 k)) = [Pad trunkLhsCol, Peak]:(runList' k)
+    runList' (Free (Branch n k)) = [Pad (trunkLhsCol - (n + 1)), LBranch n, STrunk, RBranch n]:(runList' k)
+    runList' (Free (Trunk k)) = [Pad trunkLhsCol, STrunk]:(runList' k)
+    runList' (Free (Done)) = []
+    runList' (Pure _) = []
+
 
 tree :: TreeM ()
 tree = do
   forM_ [0, 2..40] $ \n -> do
-    trunk
     branch n
+    trunk
   trunk
   trunk
   trunk
@@ -50,4 +54,4 @@ tree = do
 main = do
   let commands = runList tree
   let lines = [concat (map sTree commandLine) | commandLine <- commands]
-  mapM putStrLn $ centerlines lines
+  mapM putStrLn lines
